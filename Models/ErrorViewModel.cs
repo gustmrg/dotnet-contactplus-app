d626475c431f1568a9055901876c2cd58076d340
Gustavo Miranda <gmiranda90@outlook.com>
2022-09-23 17:53:54 -0300

    Criação do projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactPlus.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
